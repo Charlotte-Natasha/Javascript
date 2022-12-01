66dec84141a668b8b9ccac1443840ffd2b4f3738
Charlotte Natasha <natashaonyango97@gmail.com>
2022-12-01 10:35:22 +0100

    worked on loops and conditionals

---
+++ loops.js
@@ -0,0 +1,36 @@
+// Finding large number out of two
+let integer = max(370, 700)
+
+function max(integer1, integer2){
+    if(integer1 > integer2)
+
+    return integer1;
+    return integer2;
+}
+
+console.log(integer)
+
+// Take 2
+// let input1 = 400;
+// let input2 = 900;
+
+// if (input1 == input2){
+//     console.log(input1 + " is equal to " + input2)
+// }
+// elseif (input1 > input2){
+//     console.log(input1 + " is greater than " +input2)
+// }
+// else{
+//     console.log(input1 + "is less than " + input2)
+// }
+
+// Check even or odd number
+
+let number = 10;
+if (number % 2 == 0){
+    console.log(number + " is an even number ")
+}
+else{
+    console.log(number + " is an odd number ")
+}
+
