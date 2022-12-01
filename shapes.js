b57ab4d0fa7119ad0e77c6a86648fb26753a6485
Charlotte Natasha <natashaonyango97@gmail.com>
2022-12-01 09:24:53 +0100

    did the shapes sums

---
+++ shapes.js
@@ -0,0 +1,26 @@
+// Diagonal of a square
+
+// Area of a triangle
+const sideOne = 5;
+const sideTwo = 6;
+const sideThree = 7;
+
+const summation = (sideOne * sideTwo * sideThree)/2;
+
+console.log(summation)
+
+// SA of a circle
+const radius = 4;
+const pi = 22/7;
+
+const surfaceArea = (pi * radius * radius);
+
+console.log(surfaceArea)
+
+// Circumference of a circle
+const radii = 4;
+const pie = 22/7;
+
+const Circumference = (2 * pie * radii);
+
+console.log(Circumference)
